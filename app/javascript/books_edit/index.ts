1c2d66ba1e7bacfd6ffffaff671554b02b2f1b08
sergocap <systemofadown.2013@yandex.ru>
2017-09-26 13:07:08 +0700

    production mode fix

--- app/javascript/books_edit/index.ts
+++ app/javascript/books_edit/index.ts
@@ -2,7 +2,14 @@ import './polyfills.ts';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { enableProdMode  } from '@angular/core';
 
 document.addEventListener('DOMContentLoaded', () => {
   platformBrowserDynamic().bootstrapModule(AppModule);
 });
+
+if (!/localhost/.test(document.location.host)) {
+  enableProdMode();
+  console.log('enabled production mode')
+  }
+}
--- app/javascript/books_index/index.ts
+++ app/javascript/books_index/index.ts
@@ -2,7 +2,15 @@ import './polyfills.ts';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { enableProdMode  } from '@angular/core';
 
 document.addEventListener('DOMContentLoaded', () => {
   platformBrowserDynamic().bootstrapModule(AppModule);
 });
+
+
+if (!/localhost/.test(document.location.host)) {
+  enableProdMode();
+  console.log('enabled production mode')
+  }
+}
